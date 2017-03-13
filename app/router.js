0bd084bc400171019230329af720aa1873690506
Vincent Zhang <vzhang@purdue.edu>
2017-03-12 21:23:48 -0400

    Basic no-card welcome

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('cards', function() {
+    this.route('new');
+    this.route('import');
+  });
+  this.route('card', { path: '/card/:card_id' });
 });
 
 export default Router;
