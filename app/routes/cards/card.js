4e4ca5a238c3837f37d84c0c616d1255ced10322
Vincent Zhang <vzhang@purdue.edu>
2017-03-13 18:21:27 -0400

    Add size warning, set up layout, card list

--- app/routes/cards/card.js
+++ app/routes/cards/card.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.get('store').findRecord('card', params.card_id);
+        return this.get('store').findRecord('statcard', params.card_id);
     }
 });
