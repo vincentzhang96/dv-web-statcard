b8c830a2f47544e82076f62bd4a718914380153f
Vincent Zhang <vzhang@purdue.edu>
2017-03-13 21:01:42 -0400

    Start card content

---
+++ app/helpers/format-class.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Helper.extend({
+    
+    compute(params, hash) {
+
+        return "TEST";
+    }
+});
---
+++ tests/unit/helpers/format-class-test.js
@@ -0,0 +1,12 @@
+
+import { formatClass } from 'dv-web-statcard/helpers/format-class';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format class');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatClass([42]);
+  assert.ok(result);
+});
+
