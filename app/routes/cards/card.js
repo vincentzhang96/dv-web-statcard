28912c1064971035ad6a61f6f98e265caab814df
Vincent Zhang <vzhang@purdue.edu>
2017-03-18 14:28:01 -0400

    Go to card root on error

--- app/routes/cards/card.js
+++ app/routes/cards/card.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.get('store').findRecord('statcard', params.card_id);
+    },
+    actions: {
+        error(error, transition) 
+        {
+            this.replaceWith('cards');
+        }
     }
 });
