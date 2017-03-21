10225d0ba5a9911628683c62bb8ceb29ef403b15
Vincent Zhang <vzhang@purdue.edu>
2017-03-21 19:30:17 -0400

    v0.8.5-LAB

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/tool-store/prod/dv-web-statcard/0.4.7-LAB';
+    ENV.rootURL = '/tool-store/prod/dv-web-statcard/0.8.5-LAB';
     ENV.locationType = 'hash';
   }
   return ENV;
