e79a4f5a5d5c8ae91b0acb4d6670f10dc9a2b5d5
Vincent Zhang <vzhang@purdue.edu>
2017-04-07 12:28:40 -0400

    Better error messaging and import from dngs

--- app/controllers/cards/import.js
+++ app/controllers/cards/import.js
@@ -1,15 +1,47 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    queryParams: ['dngsimport'],
+    dngsimport: null,
     error: false,
+    errorMsgKey: null,
 
     onCardCreated(newCard)
     {
         this.transitionToRoute('cards.card', newCard);
     },
-    onCardImportError(err)
+    onCardImportError(err, key)
     {
         this.set('error', true);
+        this.set('errMsgKey', key)
+    },
+    loadDNGSImport()
+    {
+        if (this.get('dngsimport') !== null)
+        {
+            try {
+                let decoded = atob(this.get('dngsimport'));
+                this.processCard(decoded, "ui.cards.import.error.dngsbad");
+            } catch (err) {
+                console.log(err);
+                this.onCardImportError(err, "ui.cards.import.error.dngsbad");
+            }
+        }
+    },
+    processCard(jsonStr, key)
+    {
+        try {
+            let hash = JSON.parse(jsonStr);
+            delete hash.id;
+            let newCard = this.get('store').createRecord('statcard', hash);
+            newCard.set('lastUpdated', new Date());
+            newCard.validateAndFixModel();
+            newCard.save();
+            this.onCardCreated(newCard);
+        } catch (err) {
+            console.log(err);
+            this.onCardImportError(err, key);
+        }
     },
     actions: 
     {
@@ -19,19 +51,8 @@ export default Ember.Controller.extend({
             let reader = new FileReader();
             reader.onload = (file) => 
             {
-                try {
-                    console.log(`Loading ${fi.name}`);
-                    let hash = JSON.parse(reader.result);
-                    delete hash.id;
-                    let newCard = this.get('store').createRecord('statcard', hash);
-                    newCard.set('lastUpdated', new Date());
-                    newCard.validateAndFixModel();
-                    newCard.save();
-                    this.onCardCreated(newCard);
-                } catch (err) {
-                    console.log(err);
-                    this.onCardImportError(err);
-                }
+                console.log(`Loading ${fi.name}`);
+                this.processCard(reader.result, "ui.cards.import.error.badcard");
             };
             reader.readAsText(fi);
         }
